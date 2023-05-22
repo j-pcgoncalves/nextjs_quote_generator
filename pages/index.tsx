5dd3b0084688fca0bf68a65e327e88d6a9c7b8cc
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2023-05-22 21:18:15 +0100

    create a footer and install material UI

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,25 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '@/styles/Home.module.css'
+import React, { useState } from 'react'
 
 // Components
-import { BackgroundImage1, BackgroundImage2, GradientBackgroundCon } from '@/components/QuoteGenerator/QuoteGeneratorElements'
+import { 
+  BackgroundImage1, 
+  BackgroundImage2, 
+  FooterCon, 
+  FooterLink, 
+  GradientBackgroundCon, 
+  RedSpan
+} from '@/components/QuoteGenerator/QuoteGeneratorElements'
 
 // Assets
 import Clouds1 from "@/assets/cloud-and-thunder.png"
 import Clouds2 from "@/assets/cloudy-weather.png"
 
 export default function Home() {
+  const [numberOfQuotes, setNumberOfQuotes] = useState<Number | null>(0);
+
   return (
     <>
       <Head>
@@ -32,6 +42,20 @@ export default function Home() {
           height="300"
           alt="cloudybackground2"
         />
+
+        <FooterCon>
+          <>
+            Quotes Generated: {numberOfQuotes}
+            <br />
+            Developed with <RedSpan>♥</RedSpan> by <FooterLink 
+                                  href="https://github.com/j-pcgoncalves" 
+                                  target="_blank" 
+                                  rel="noopener"
+                                > 
+                                  João Gonçalves 
+                                </FooterLink>
+          </>
+        </FooterCon>
         
       </GradientBackgroundCon>
     </>
